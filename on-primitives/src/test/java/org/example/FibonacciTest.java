a1966de3ec54fd3383d6e2e4eba4e9b3fe19a4c7
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2024-11-23 12:50:56 -0300

    update: improving with parameterized testing.

--- on-primitives/src/test/java/org/example/FibonacciTest.java
+++ on-primitives/src/test/java/org/example/FibonacciTest.java
@@ -1,31 +1,56 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.ParameterizedTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import java.util.function.IntFunction;
+import java.util.stream.Stream;
 
 
 public class FibonacciTest {
-
-    @Test
-    public void testAllFibonacci() {
-        testFibonacci(Fibonacci::linear);
-        testFibonacci(Fibonacci::tail);
-        testFibonacci(Fibonacci::multiple);
-        testFibonacci(Fibonacci::mutual);
-        testFibonacci(Fibonacci::nested);
-    }
-    
-    private void testFibonacci(IntFunction<Integer> f) {
-        assertAll(
-            () -> assertEquals(0, f.apply(0)),
-            () -> assertEquals(1, f.apply(1)),
-            () -> assertEquals(1, f.apply(2)),
-            () -> assertEquals(13, f.apply(7)),
-            () -> assertEquals(21, f.apply(8)),
-            () -> assertEquals(55, f.apply(10))
+    private static Stream<Arguments> fibonacciInputs() {
+        return Stream.of(
+            Arguments.of(0, 0),
+            Arguments.of(1, 1),
+            Arguments.of(1, 2),
+            Arguments.of(2, 3),
+            Arguments.of(3, 4),
+            Arguments.of(5, 5),
+            Arguments.of(8, 6),
+            Arguments.of(13, 7),
+            Arguments.of(21, 8),
+            Arguments.of(55, 10)
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("fibonacciInputs")
+    public void testFibonacciLinear(int expected, int n) {
+        assertEquals(expected, Fibonacci.linear(n));
+    }
+
+    @ParameterizedTest
+    @MethodSource("fibonacciInputs")
+    public void testFibonacciTail(int expected, int n) {
+        assertEquals(expected, Fibonacci.tail(n));
+    }
+
+    @ParameterizedTest
+    @MethodSource("fibonacciInputs")
+    public void testFibonacciMultiple(int expected, int n) {
+        assertEquals(expected, Fibonacci.multiple(n));
+    }
+
+    @ParameterizedTest
+    @MethodSource("fibonacciInputs")
+    public void testFibonacciMutual(int expected, int n) {
+        assertEquals(expected, Fibonacci.mutual(n));
+    }
+
+    @ParameterizedTest
+    @MethodSource("fibonacciInputs")
+    public void testFibonacciNested(int expected, int n) {
+        assertEquals(expected, Fibonacci.nested(n));
+    }    
 }
